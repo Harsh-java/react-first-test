b85d64ac09294526cf6c95a32fc177bb8ddaca9e
Harsh-java <itsharshchourasiya@gmail.com>
2024-06-27 14:40:42 +0000

    change

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('displays Hello, World! when button is clicked', () => {
   render(<App />);
